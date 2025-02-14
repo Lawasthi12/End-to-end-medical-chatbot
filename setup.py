b89c4d70f2da50029cc78d3a643998a783f9840a
Lawasthi12 <lawasthi12@gmail.com>
2025-02-14 23:15:09 +0800

    setup updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version = '1.0.0',
+    author= 'asd',
+    author_email= 'lawasthi12@',
+    packages= find_packages(),
+    install_requires = []
+
+)
